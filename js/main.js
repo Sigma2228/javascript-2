dd907ba06915a1e1de691e6ae1e83eaf499c0b75
Sigma2228 <149525498+Sigma2228@users.noreply.github.com>
2024-10-28 13:53:52 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+let c = 25; 
+
+let f = (25 * 9/5 + 32)
+console.log('25 c = ' + f + ' f' )
+
+const days = 30
+const hours = 24
+const minute = 60
+const hoursInMonth =  days*hours
+const minutesInMonth = (minute*hours)*days
+console.log(hoursInMonth)
+console.log(minutesInMonth)
+
+const level = 20
+const energy = 100
+const health = 100
+const damage = 30
+const energylost = 40
+const currentHealth = health-damage
+const currentEnergy = energy-energylost
+console.log(currentHealth + 'Hp')
+console.log(currentEnergy + 'E')
+
+const suma = 200
+const sale = suma/10
+const sumaWithSale = 180
+console.log(sumaWithSale + ' Грн')
+
+const number = 2.28
+const float = Math.floor(number)
+console.log(float)
+
+
+const floatString = "14.88"
+const floatNumber = Number.parseFloat(floatString)
+console.log(floatNumber)
+
+
+const numberString = "9"
+const parseint = Number.parseInt(numberString)
+console.log(parseint)
+
+const sqrt = 16
+const math = Math.sqrt(sqrt)
+console.log(math)
+
+
+const wholeNumber = 7
+const stringNumber = '3'
+const parseIntNumber = Number.parseInt(stringNumber)
+const toStringNumber = wholeNumber.toString()
+console.log(parseIntNumber)
+console.log(toStringNumber)
+
+
